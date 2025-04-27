3ea97460bf2d4fad53c237a1409a5b49d487b000
Shiran_lakshitha <shiranlakshithareal@gmail.com>
2025-04-27 21:28:30 +0530

    search function added

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import '../components/Weather.css'
 import search_icon from '../assets/search.png'
 import clear_icon from '../assets/clear.png'
@@ -12,6 +12,8 @@ import humidity_icon from '../assets/humidity.png'
 
 const Weather = () => {
 
+
+    const inputRef = useRef()
     
     const [weatherData, setWeatherData] = useState(false)
 
@@ -34,11 +36,24 @@ const Weather = () => {
     }
 
     const search = async (city) => {
+
+        if(city === "") {
+            alert("enter city name")
+            return
+        }
+
         try {
             const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${import.meta.env.VITE_APP_ID}`
             
             const response = await fetch(url)
             const data = await response.json()
+
+            if(!response.ok) {
+                alert(data.message)
+                return
+            }
+
+
             const icon = allIcons[data.weather[0].icon] || clear_icon
             console.log(data)
             setWeatherData({
@@ -48,8 +63,11 @@ const Weather = () => {
                 location: data.name,
                 icon: icon
             })
+
+
         } catch (error) {
-            console.log(error)
+            setWeatherData(false)
+            console.error('error in fetching weather data', error)
         }
     }
 
@@ -61,10 +79,11 @@ const Weather = () => {
   return (
     <div className='weather'>
         <div className="search-bar">
-            <input type="text" placeholder='Search' />
-            <img src={search_icon} alt="" />
+            <input ref={inputRef} type="text" placeholder='Search' />
+            <img src={search_icon} onClick={() => search(inputRef.current.value)} alt="" />
         </div>
-        <img src={weatherData.icon} className='weather-icon' alt="" />
+        {weatherData ? <>
+            <img src={weatherData.icon} className='weather-icon' alt="" />
         <p className='temperature'>{weatherData.temperature}°C</p>
         <p className='location'>{weatherData.location}</p>
         <div className="weather-data">
@@ -82,7 +101,8 @@ const Weather = () => {
                     <span>Wind Speed</span>
                 </div>
             </div>  
-        </div>
+        </div></>: <></>}
+        
     </div>
   )
 }
